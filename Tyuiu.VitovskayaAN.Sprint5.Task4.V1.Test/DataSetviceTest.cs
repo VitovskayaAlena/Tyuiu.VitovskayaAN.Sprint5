5fb2242f3e160c7c4bd25a8b69111c895b1b5a74
qqbelll <alenkav07@listru>
2025-11-27 00:48:54 +0500

    Task4

---
+++ Tyuiu.VitovskayaAN.Sprint5.Task4.V1.Test/DataSetviceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.VitovskayaAN.Sprint5.Task4.V1.Lib;
+namespace Tyuiu.VitovskayaAN.Sprint5.Task4.V1.Test
+{
+    [TestClass]
+    public sealed class DataSetviceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask4V1.txt");
+            FileInfo fileInfo = new FileInfo(path);
+            double wait = -3.68;
+            double res = ds.LoadFromDataFile(path);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VitovskayaAN.Sprint5.Task4.V1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.VitovskayaAN.Sprint5.Task4.V1.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Витовская А. Н. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Чтение данных из текстового файла                                       *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнила: Витовская Алёна Николаевна | РППб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формулу.            *");
+        Console.WriteLine("* Вычислить значение и вывести на консоль. Округлить до 3 знаков.         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+        string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask4V1.txt");
+        Console.WriteLine("Данные находятся в файле: " + path);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+    }
+}
