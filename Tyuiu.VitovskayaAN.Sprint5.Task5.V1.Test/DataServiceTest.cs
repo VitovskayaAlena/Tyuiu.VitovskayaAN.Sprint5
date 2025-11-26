23845ff80702cb8bee3c62647e2ccdeed3f77046
qqbelll <alenkav07@listru>
2025-11-27 01:08:13 +0500

    Task5

---
+++ Tyuiu.VitovskayaAN.Sprint5.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.VitovskayaAN.Sprint5.Task5.V1.Lib;
+using System.IO;
+namespace Tyuiu.VitovskayaAN.Sprint5.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool file = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, file);
+        }
+    }
+}
